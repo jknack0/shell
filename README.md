b86b433dd3de877254fce5e60271a9d399941315
Anthony <ajsouza88@gmail.com>
2019-06-25 22:26:17 -0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# csc415-p3-jknack0
+# CSC 415 - Project 3 - My Shell
+
+## Student Name: Name here
+
+## Student ID: ID Here
+
+## Build Instructions
+
+## Run Instructions
+
+## List Extra Credit comepleted (if not attempted leave blank)
---
+++ myshell.c
@@ -0,0 +1,32 @@
+/****************************************************************
+ * Name        :                                                *
+ * Class       :  CSC 415                                       *
+ * Date        :                                                *
+ * Description :  Writting a simple bash shell program          *
+ *                that will execute simple commands. The main   *
+ *                goal of the assignment is working with        *
+ *                fork, pipes and exec system calls.            *
+ ****************************************************************/
+
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+
+/* CANNOT BE CHANGED */
+#define BUFFERSIZE 256
+/* --------------------*/
+#define PROMPT "myShell >> "
+#define PROMPTSIZE sizeof(PROMPT)
+
+int 
+main(int argc, char** argv)
+{
+
+    
+return 0;
+}
